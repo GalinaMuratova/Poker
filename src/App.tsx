4ce836efe27e513ead3336b0f629cfdef66d8425
GalinaMuratova <ufkz200147@gmail.com>
2023-05-22 14:29:56 +0600

    Второй уровень работает.Создала классы для логики колоды. В App.tsx добавила строчки что бы проверить.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import './App.css';
 import CardView from "./CardView/card-view";
+import CardDeck from "./lib/CardDeck";
 
 function App() {
+    const cardDeck = new CardDeck();
+    console.log(cardDeck.deck);
+    console.log(cardDeck.getCards(5));
+    console.log(cardDeck.deck);
+    console.log(cardDeck.getCards(5))
   return (
     <div className="App">
         <div className="playingCards faceImages">
             <CardView rank="K" suit="diams" />
             <CardView rank="Q" suit="hearts" />
-            <CardView rank="K" suit="clubs" />
-            <CardView rank="K" suit="spades" />
-            <CardView rank="K" suit="clubs" />
+            <CardView rank="2" suit="clubs" />
+            <CardView rank="10" suit="spades" />
+            <CardView rank="A" suit="clubs" />
         </div>
     </div>
   );
